aadd46dc79fcab13d9ad4143e773dda451e35813
JunBread <junbbang5@gmail.com>
2020-03-21 18:58:10 +0900

    Revise code structure, support PDF/download

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,14 +1,119 @@
 window.onload = function () {
-    chrome.extension.onMessage.addListener((msg, sender, sendResponse) => {
-        if (msg === 'get-video') {
-            let id = document.querySelector('html').getAttribute('id')
-            let title = document.title
-            let isVideo = document.querySelector('#video-player')
-            if (id && isVideo && id.match(/\w+-page/)) {
-                id = id.replace('-page', '')
-                console.log({ id, title })
-                chrome.runtime.sendMessage({ id, title })
+    function getFromCookie(cookieName) {
+        const query = document.cookie.match(`(^|;) ?${cookieName}=([^;]*)(;|$)`)
+        return query ? query[2] : null
+    }
+
+    function getFromFormField(fieldName) {
+        const form = document.querySelector('#tool_form')
+        return form ? form.querySelector(`[name="${fieldName}"]`).value : null
+    }
+
+    function makeApiRequest(url, apiToken, callback) {
+        const request = new XMLHttpRequest()
+
+        request.onreadystatechange = () => {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 200) {
+                    callback(request.responseText)
+                }
             }
         }
+
+        request.open('GET', url)
+        request.setRequestHeader('Authorization', `Bearer ${apiToken}`)
+        request.send()
+    }
+
+    function isTopFrame() {
+        let topFrame = false
+        try {
+            topFrame = top.document === window.document
+        } catch (e) { }
+
+        return topFrame
+    }
+
+    function checkMode() {
+        if (document.querySelector('[id^=xn-unit')) return 'unit'
+        if (document.querySelector('[id^=xn-course')) return 'course'
+        
+        return 'etc'
+    }
+
+    chrome.extension.onMessage.addListener((msg, sender, sendResponse) => {
+        if (isTopFrame()) {
+            const apiToken = getFromCookie('xn_api_token')
+            const userId = getFromFormField('custom_user_id')
+            const userLogin = getFromFormField('custom_user_login')
+            const courseCode = getFromFormField('custom_course_id')
+
+            if (msg === 'get-contents') {
+                if (apiToken && courseCode) {
+                    makeApiRequest(`https://canvas.skku.edu/learningx/api/v1/courses/${courseCode}/allcomponents_db?user_id=${userId}&user_login=${userLogin}&role=1`, apiToken, text => {
+                        let files = JSON.parse(text)
+                        files = files.map(file => {
+                            return {
+                                id: file.commons_content.content_id,
+                                title: file.title,
+                                type: file.commons_content.content_type,
+                                url: file.commons_content.view_url
+                            }
+                        })
+                        chrome.runtime.sendMessage({ type: 'get-contents', data: files })
+                    })
+                }
+            }
+
+            if (msg === 'get-resources') {
+                if (apiToken && courseCode) {
+                    makeApiRequest(`https://canvas.skku.edu/learningx/api/v1/courses/${courseCode}/resources_db`, apiToken, text => {
+                        let files = JSON.parse(text)
+                        files = files.map(file => {
+                            return {
+                                id: file.commons_content.content_id,
+                                title: file.title,
+                                type: file.commons_content.content_type,
+                                url: file.commons_content.view_url
+                            }
+                        })
+                        chrome.runtime.sendMessage({ type: 'get-resources', data: files })
+                    })
+                }
+            }
+        }
+
+        if (msg === 'get-content') {
+            let id = document.querySelector('html').getAttribute('id')
+            let title = document.title
+
+            if (id && id.match(/\w+-page/)) {
+                id = id.replace('-page', '')
+                let type = document.querySelector('meta[name="commons.content_type"]')
+                if (type) {
+                    switch (type.getAttribute("content")) {
+                        case "2":
+                            type = 'movie'
+                            break
+                        case "10":
+                            type = 'pdf'
+                            break
+                        default:
+                            type = 'etc'
+                            break
+                    }
+
+                    chrome.runtime.sendMessage({ type: 'get-content', data: { id, title, type } })
+                }
+
+            }
+        }
+
+        if (msg === 'check-mode') {
+            const pageMode = checkMode()
+            if (pageMode !== 'etc')
+                chrome.runtime.sendMessage({ type: 'check-mode', data: pageMode })
+        }
+
     })
 }
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,23 +1,134 @@
 window.onload = () => {
+    function findValidContentsRepository(url, callback) {
+        const contentRepositories = ["contents", "contents2", "contents3"]
+        contentRepositories.forEach(repository => {
+            const repositoryUrl = `https://cdn-lcms.skku.edu/${repository}/skku100001/${url}`
 
-    const vidList = document.querySelector('#vid-list')
+            const request = new XMLHttpRequest()
+            request.onreadystatechange = () => {
+                if (request.readyState === XMLHttpRequest.DONE) {
+                    if (request.status === 200) {
+                        callback(repositoryUrl)
+                    }
+                }
+            }
 
-    chrome.runtime.onMessage.addListener((video, sender, sendResponse) => {
-        document
-            .querySelectorAll('.msg-error')
-            .forEach(el => el.setAttribute("style", "display:none"))
+            request.open('HEAD', repositoryUrl)
+            request.send()
+        })
+    }
 
-        let vidElem = document.createElement('li')
-        vidElem.setAttribute("id", video.id)
-        vidElem.innerText = video.title
-        vidList.appendChild(vidElem)
+    const contents = document.querySelector('#contents')
+    const resources = document.querySelector('#resources')
+    const contentList = document.querySelector('#content-list')
+    const resourceList = document.querySelector('#resource-list')
 
-        document.getElementById(video.id).addEventListener('click', () => {
-            chrome.tabs.create({ url: `https://cdn-lcms.skku.edu/contents3/skku100001/${video.id}/contents/media_files/mobile/ssmovie.mp4` })
+    let pageMode = 'etc'
+
+    document.querySelector('#resource-download-all').addEventListener('click', () => {
+        document.querySelectorAll('#resource-list li').forEach(el => {
+            const id = el.getAttribute('id')
+            const type = el.getAttribute('data-type')
+            const filename = encodeURIComponent(el.innerText) + '.pdf'
+
+            findValidContentsRepository(`${id}/contents/web_files/original.pdf`, url => {
+                chrome.downloads.download({ url, filename })
+            })
         })
     })
 
+    chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+        if (msg.type === 'check-mode') {
+            pageMode = msg.data
+
+            chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+                const currentUrl = tabs[0].url
+                if (currentUrl.match(/.+\/external_tools\/1.*/)) { // contents
+                    contents.setAttribute('style', 'display: block')
+                    resources.setAttribute('style', 'display: none')
+                    if (pageMode === 'course')
+                        chrome.tabs.sendMessage(tabs[0].id, 'get-contents')
+                    if (pageMode === 'unit')
+                        chrome.tabs.sendMessage(tabs[0].id, 'get-content')
+                }
+                if (currentUrl.match(/.+\/external_tools\/2.*/)) { // resouces
+                    contents.setAttribute('style', 'display: none')
+                    resources.setAttribute('style', 'display: block')
+                    chrome.tabs.sendMessage(tabs[0].id, 'get-resources')
+                }
+            })
+        }
+
+        if (msg.type.match('get-contents?$')) {
+            function addContentRow(file) {
+                let contentElem = document.createElement('li')
+                contentElem.setAttribute("id", file.id)
+                contentElem.innerText = file.title
+                contentList.appendChild(contentElem)
+
+                document.getElementById(file.id).addEventListener('click', () => {
+                    if (file.type.match('movie'))
+                        findValidContentsRepository(`${file.id}/contents/media_files/mobile/ssmovie.mp4`, url => {
+                            chrome.tabs.create({ url })
+                        })
+                    else if (file.type.match('screenlecture'))
+                        findValidContentsRepository(`${file.id}/contents/media_files/mixed.mp4`, url => {
+                            chrome.tabs.create({ url })
+                        })
+                    else if (file.type.match('pdf'))
+                        findValidContentsRepository(`${file.id}/contents/web_files/original.pdf`, url => {
+                            chrome.tabs.create({ url })
+                        })
+                    else
+                        chrome.tabs.create({ url: file.url })
+                })
+            }
+
+            if (msg.type === 'get-content') {
+                document.querySelector('#err-no-contents').setAttribute('style', 'display:none')
+                addContentRow(msg.data)
+            }
+
+            if (msg.type === 'get-contents') {
+                if (msg.data.length)
+                    document.querySelector('#err-no-contents').setAttribute('style', 'display:none')
+                msg.data.forEach(file => { addContentRow(file) })
+
+            }
+        }
+
+        if (msg.type === 'get-resources') {
+            if (msg.data.length)
+                document.querySelector('#err-no-resources').setAttribute('style', 'display:none')
+
+            msg.data.forEach(file => {
+                let resourceElem = document.createElement('li')
+                resourceElem.setAttribute("id", file.id)
+                resourceElem.setAttribute("data-type", file.type)
+                resourceElem.innerText = file.title
+                resourceList.appendChild(resourceElem)
+
+                document.getElementById(file.id).addEventListener('click', () => {
+                    if (file.type.match('movie'))
+                        findValidContentsRepository(`${file.id}/contents/media_files/mobile/ssmovie.mp4`, url => {
+                            chrome.tabs.create({ url })
+                        })
+                    else if (file.type.match('screenlecture'))
+                        findValidContentsRepository(`${file.id}/contents/media_files/mixed.mp4`, url => {
+                            chrome.tabs.create({ url })
+                        })
+                    else if (file.type.match('pdf'))
+                        findValidContentsRepository(`${file.id}/contents/web_files/original.pdf`, url => {
+                            chrome.tabs.create({ url })
+                        })
+                    else
+                        chrome.tabs.create({ url: file.url })
+                })
+            })
+        }
+    })
+
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, 'get-video')
+        chrome.tabs.sendMessage(tabs[0].id, 'check-mode')
     })
 }
